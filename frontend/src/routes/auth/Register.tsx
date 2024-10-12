3fc0c4e572b183cc8e9f671b082ab7e5dbaf9cce
Minje Sung <minje813@dgu.ac.kr>
2024-10-13 02:53:48 +0900

    FE: [feat] checkID API 연동

--- frontend/src/routes/auth/Register.tsx
+++ frontend/src/routes/auth/Register.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+import { checkID } from "../../api/authAPI"
+
 export default function Register() {
+    const [id, setId] = useState('')
+    const [password, setPassword] = useState('')
     return (
-        <div>회원가입페이지임</div>
+        <div>
+            <input type="text" onChange={(e) => { setId(e.target.value) }}></input>
+            <input type="password" onChange={(e) => { setPassword(e.target.value) }}></input>
+            <button onClick={()=>{
+                checkID(id)
+            }}>아이디 중복 확인</button>
+        </div>
     )
 }
