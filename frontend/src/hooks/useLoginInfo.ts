fea98c3b906b08bec9fc17b79d047d02c06803d2
Minje Sung <minje813@dgu.ac.kr>
2024-10-21 18:50:50 +0900

    FE: [feat] useLoginInfo hook 추가

---
+++ frontend/src/hooks/useLoginInfo.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const useLoginInfo = () => {
+    // 로그인 정보 확인
+    const [isLogin, setIsLogin] = useState(false)
+    const [accessToken, setAccessToken] = useState("")
+    
+    useEffect(() => {
+        const accessTokenCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('access='));
+        const accessTokenValue = accessTokenCookie ? accessTokenCookie.split('=')[1] : '';
+        if (accessTokenCookie) setIsLogin(true)
+        setAccessToken(accessTokenValue)
+    }, [])
+
+    return [isLogin, accessToken]
+}
