a90d13a1cd209de4c09c9ab168b591923564f8ee
Kim Bum Soo <bumsookim333@naver.com>
2024-10-23 17:49:49 +0900

    BE: [feat] 프로젝트 찾기 기능

--- backend/team-sync/src/main/java/com/pocketstone/team_sync/controller/ProjectController.java
+++ backend/team-sync/src/main/java/com/pocketstone/team_sync/controller/ProjectController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -27,5 +26,22 @@ public class ProjectController {
         ProjectDto projectDto = projectWrapperDto.getProjectDto();
         return new ResponseEntity<>(projectService.save(projectDto), HttpStatus.CREATED);
     }
-    //@GetMapping
+
+
+    //프로젝트 이름으로 프로젝트 찾기
+    @GetMapping("/{projectName}")
+    public ResponseEntity<ProjectDto> getProject(@PathVariable String projectName) {
+        return projectService.findByProjectName(projectName)
+                .map(project -> new ResponseEntity<>(project, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    //모든 프로젝트 찾기
+    @GetMapping("/all")
+    public ResponseEntity <List<ProjectDto>> getAllProjects() {
+        return new ResponseEntity<>(projectService.findAll(), HttpStatus.OK);
+    }
+    //@PostMapping 프로젝트 업데이트
+    //@DeleteMapping 프로젝트 삭제
+    //@GetMapping 프로젝트 시작일과 MVP일로 프로젝트 찾기
+
 }
