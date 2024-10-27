fd37e3eba313cfabd2514715cc5bad18ad76949b
Minje Sung <minje813@dgu.ac.kr>
2024-10-28 02:15:02 +0900

    FE: [refactor] 파일 이름 변경 및 ErrorMessage 분리

--- frontend/src/api/submitExcelData.ts
+++ frontend/src/api/submitExcelData.ts
@@ -1,15 +1,16 @@
 import { API_URL } from '../constants/envText';
+import { NO_EXCEL_FILE } from '../constants/errorMessage';
 import { tokenAxios } from './tokenAPI';
 
 // FormData 생성 및 서버로 전송
-export const handleSubmit = async (
+export const submitExcelData = async (
   e: React.FormEvent<HTMLFormElement>,
   file: File | null
 ) => {
   e.preventDefault();
 
   if (!file) {
-    alert('파일을 선택하세요.');
+    alert(NO_EXCEL_FILE);
     return;
   }
 
