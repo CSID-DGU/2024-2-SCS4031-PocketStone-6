b6263bf0476c968b77570e63f7af2d4c74b15691
Kim Bum Soo <bumsookim333@naver.com>
2024-11-10 02:04:34 +0900

    BE:[fix] 차터pdf업로드 Exception핸들러 버그 수정

--- backend/team-sync/src/main/java/com/pocketstone/team_sync/utility/ApplicationExceptionHandler.java
+++ backend/team-sync/src/main/java/com/pocketstone/team_sync/utility/ApplicationExceptionHandler.java
@@ -4,8 +4,12 @@ package com.pocketstone.team_sync.utility;
 import com.pocketstone.team_sync.exception.*;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -33,14 +37,21 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler({ConstraintViolationException.class, CharterUploadException.class})
-    protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
         Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
-        List<String> errorMessages = violations.stream()
+        List<String> error = violations.stream()
                 .map(ConstraintViolation::getMessage)
                 .collect(Collectors.toList());
-        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(CharterUploadException.class)
+    public ResponseEntity<Object> handleCharterUploadException(CharterUploadException ex) {
+        ErrorResponse error = new ErrorResponse(Collections.singletonList(ex.getMessage()));
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
 
 
