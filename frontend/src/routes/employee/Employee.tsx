2060524388da00586293bf9175d7976d914f290f
Minje Sung <minje813@dgu.ac.kr>
2024-11-15 18:23:56 +0900

    FE: [feat] 모든 사원 정보 삭제 API 연동

--- frontend/src/routes/employee/Employee.tsx
+++ frontend/src/routes/employee/Employee.tsx
@@ -3,14 +3,17 @@ import ExcelUploader from '../../components/Input/ExcelUploader';
 import { useAllEmployeeInfoQuery } from '../../hooks/useAllEmployeeInfoQuery';
 import { deleteOneEmployee } from '../../api/employee/deleteOneEmployee';
 import { refreshPage } from '../../utils/movePage';
+import { deleteAllEmployInfo } from '../../api/employee/deleteAllEmployInfo';
 
 export default function Employee() {
   const allEmployInfoQuery = useAllEmployeeInfoQuery();
+  const navigate = useNavigate();
 
   return (
     <div>
       <h2>Employee</h2>
       <ExcelUploader />
+      <button onClick={() => deleteAllEmployInfo(navigate)}>사원정보 삭제</button>
       {allEmployInfoQuery.data?.map(
         ({ employeeId, staffId, name, departmeent, position }: employeeInfoType, i: number) => (
           <EmployBlock
