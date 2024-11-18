35eb14390eee5d5a20a42a76aef120c2153ee1ea
Minje Sung <minje813@dgu.ac.kr>
2024-11-18 17:29:04 +0900

    FE: [feat] 인원관리 페이지 스타일 적용

--- frontend/src/routes/employee/EmployeeDetail.tsx
+++ frontend/src/routes/employee/EmployeeDetail.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useOneEmployeeInfoQuery } from '../../hooks/useOneEmployeeInfoQuery';
 import { useOneEmployeeSpecQuery } from '../../hooks/useOneEmployeeSpecQuery';
+import MS from 'routes/Main.module.scss'
 
 export default function EmployeeDetail() {
   const { id } = useParams();
@@ -8,7 +9,7 @@ export default function EmployeeDetail() {
   const oneEmployeeSpecQuery = useOneEmployeeSpecQuery(Number(id));
 
   return (
-    <div>
+    <div className={MS.container}>
       <p>ID: {id} 사원 디테일 정보임</p>
       {JSON.stringify(oneEmployeeInfoQuery.data)}
       {JSON.stringify(oneEmployeeSpecQuery.data)}
