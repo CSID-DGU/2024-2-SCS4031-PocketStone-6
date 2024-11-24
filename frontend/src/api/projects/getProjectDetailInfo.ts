ccca561b20e5cb9d53e872670c889a32bc88e853
Minje Sung <minje813@dgu.ac.kr>
2024-11-25 07:00:46 +0900

    FE: [feat] 프로젝트 디테일 요소 조회 API 추가

---
+++ frontend/src/api/projects/getProjectDetailInfo.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { tokenAxios } from '../tokenAPI';
+import { API_URL } from '../../constants/envText';
+import { headers } from '../../constants/headers';
+
+export const getProjectBasicInfo = async (id: number) => {
+  try {
+    const response = await tokenAxios.get(`${API_URL}/api/projects/id/${id}`, {
+      headers: headers,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      return error.response?.data;
+    }
+  }
+};
+
+export const getProjectCharter = async (id: number) => {
+  try {
+    const response = await tokenAxios.get(`${API_URL}/api/projects/charter/${id}`, {
+      headers: headers,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      return error.response?.data;
+    }
+  }
+};
+
+export const getProjectTimelines = async (id: number) => {
+  try {
+    const response = await tokenAxios.get(`${API_URL}/api/projects/timelines/${id}`, {
+      headers: headers,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      return error.response?.data;
+    }
+  }
+};
---
+++ frontend/src/api/projects/getProjectMember.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { tokenAxios } from '../tokenAPI';
+import { API_URL } from '../../constants/envText';
+import { headers } from '../../constants/headers';
+
+export const getProjectMember = async (id: number) => {
+  try {
+    const response = await tokenAxios.get(`${API_URL}/api/member/${id}`, {
+      headers: headers,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      return error.response?.data;
+    }
+  }
+};
