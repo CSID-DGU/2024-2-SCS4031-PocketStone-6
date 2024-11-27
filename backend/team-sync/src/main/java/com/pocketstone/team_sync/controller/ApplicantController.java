6a1347c98c8fa646b555894c504b9a3ac00a6815
Minbro-Kim <ab000701@dgu.edu>
2024-11-27 16:14:43 +0900

    BE:[feat] 지원자 정보 삭제

--- backend/team-sync/src/main/java/com/pocketstone/team_sync/controller/ApplicantController.java
+++ backend/team-sync/src/main/java/com/pocketstone/team_sync/controller/ApplicantController.java
@@ -2,6 +2,8 @@ package com.pocketstone.team_sync.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +30,19 @@ public class ApplicantController {
         applicantService.enrollApplicantList(user, file);
         return ResponseEntity.ok(new MessageResponseDto("등록되었습니다."));
     }
+
+     //지원자 개인 삭제
+    @DeleteMapping("/{applicantId}")
+    public ResponseEntity<MessageResponseDto> deleteApplicant(@AuthenticationPrincipal User user, @PathVariable("applicantId") Long applicantId){
+        applicantService.deleteApplicant(user, applicantId);
+        return ResponseEntity.ok(new MessageResponseDto("해당 지원자가 삭제 되었습니다."));
+    }
+
+    //지원자 전체 삭제
+    @DeleteMapping("")
+    public ResponseEntity<MessageResponseDto> deleteAllApplicant(@AuthenticationPrincipal User user ){
+        applicantService.deleteAllApplicant(user);
+        return ResponseEntity.ok(new MessageResponseDto("모든 지원자 정보가 삭제 되었습니다."));
+    }
 }
+
--- backend/team-sync/src/main/java/com/pocketstone/team_sync/repository/ApplicantRepository.java
+++ backend/team-sync/src/main/java/com/pocketstone/team_sync/repository/ApplicantRepository.java
@@ -3,7 +3,10 @@ package com.pocketstone.team_sync.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pocketstone.team_sync.entity.Applicant;
+import com.pocketstone.team_sync.entity.Company;
 
 public interface ApplicantRepository  extends JpaRepository<Applicant, Long> {
 
+    void deleteByCompany(Company company);
+    void deleteByCompanyAndId(Company company, Long employeeId);
 }
