fad3f31f816d26f1787211403bba6655ec946d2d
Minje Sung <minje813@dgu.ac.kr>
2024-12-01 22:31:05 +0900

    FE: [feat] 프로젝트 타임라인 추가기능 구현

--- frontend/src/routes/project/ProjectTimelines.tsx
+++ frontend/src/routes/project/ProjectTimelines.tsx
@@ -1,15 +1,56 @@
+import { TimelinesInput } from 'components/Input/TimelinesInput';
 import { useProjectDetailInfoQuery } from 'hooks/useProjectDetailInfoQuery';
+import { useTimelinesState } from 'hooks/useTimelinesState';
+import { FaPlus } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
-import { MS } from 'styles';
+import { BS, MS } from 'styles';
+import { addTimelines } from 'utils/addTimelines';
+import { handleTimelinesChange } from 'utils/handleTimelinesContent';
 
 export default function ProjectTimelines() {
   const { id } = useParams();
-  const { timelinesQuery } = useProjectDetailInfoQuery(Number(id));
+  const { timelinesContent, setTimelinesContent } = useTimelinesState(Number(id));
+  const { basicInfoQuery } = useProjectDetailInfoQuery(Number(id));
 
   return (
     <div className={MS.container}>
-      <p>{id} 프로젝트 타임라인</p>
-      <p>{JSON.stringify(timelinesQuery?.data)}</p>
+      <div className={MS.content}>
+        <div className={`${MS.contentTitle} ${MS.displayFlex} ${MS.flexSpace}`}>
+          <p>{id} 프로젝트 타임라인</p>
+          <div className={MS.displayFlex}>
+            <button
+              className={BS.addBtn}
+              onClick={() => {
+                setTimelinesContent(
+                  addTimelines(
+                    timelinesContent,
+                    basicInfoQuery.data?.startDate,
+                    basicInfoQuery.data?.mvpDate
+                  )
+                );
+              }}>
+              <FaPlus />
+            </button>
+            <button
+              className={BS.YellowBtn}
+              onClick={() => {
+                console.log(timelinesContent);
+              }}>
+              값 확인
+            </button>
+          </div>
+        </div>
+        <div className={MS.contentBox}>
+          {timelinesContent === undefined ? null : (
+            <TimelinesInput
+              timelinesList={[...timelinesContent.slice()]}
+              onChange={handleTimelinesChange}
+              content={timelinesContent}
+              setContent={setTimelinesContent}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/utils/filterDate.ts
+++ frontend/src/utils/filterDate.ts
@@ -1,8 +1,8 @@
-export const isMonday = (date: Date, endDate: Date|null) => {
+export const isMonday = (date: Date, endDate: Date | null) => {
   const day = date.getDay();
   const today = new Date();
 
-  if (endDate !== null) return day === 1 && today <= date && endDate > date
+  if (endDate !== null) return day === 1 && today <= date && endDate > date;
   return day === 1 && today <= date;
 };
 
@@ -10,4 +10,17 @@ export const isSunday = (date: Date, startDate: Date | null) => {
   if (startDate === null) return false;
   const day = date.getDay();
   return day === 0 && startDate < date;
+};
+
+export const isCanPickMonday = (date: Date, sprintEndDate: Date) => {
+  const day = date.getDay();
+  const today = new Date();
+
+  return day === 1 && today <= date && sprintEndDate > date;
+};
+
+export const isCanPickSunday = (date: Date, sprintStartDate: Date) => {
+  const day = date.getDay();
+
+  return day === 0 && sprintStartDate < date;
 };
