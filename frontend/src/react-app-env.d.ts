93434f949cb8e30eff908a8bd2aeb2f7bffabe1d
Minje Sung <minje813@dgu.ac.kr>
2024-12-01 18:51:26 +0900

    FE: [feat] 프로젝트 차터 추가기능 구현

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -39,47 +39,58 @@ interface RegisterContent {
   memberList: employeeIdObject[];
 }
 
+interface Objectives {
+  id?: number;
+  objectiveName: string;
+  objectiveContent: string;
+}
+
+interface Positions {
+  id?: number;
+  positionName: string;
+  positionContent: string;
+  positionCount?: number;
+}
+
+interface Principles {
+  id?: number;
+  principleName: string;
+  principleContent: string;
+}
+
+interface Scopes {
+  id?: number;
+  scopeName: string;
+  scopeContent: string;
+}
+
+interface Visions {
+  id?: number;
+  visionName: string;
+  visionContent: string;
+}
+
+interface Stakeholders {
+  id?: number;
+  stakeholderName: string;
+  stakeholderContent: string;
+}
+
+interface Risks {
+  id?: number;
+  riskName: string;
+  riskContent: string;
+}
+
 interface CharterContent {
   id?: number;
-  objectives: {
-    id?: number;
-    objectiveName: string;
-    objectiveContent: string;
-  }[];
-  positions: {
-    id?: number;
-    positionName: string;
-    positionContent: string;
-    positionCount?: number;
-  }[];
-
-  principles: {
-    id?: number;
-    principleName: string;
-    principleContent: string;
-  }[];
-  scopes: {
-    id?: number;
-    scopeName: string;
-    scopeContent: string;
-  }[];
-  visions: {
-    id?: number;
-    visionName: string;
-    visionContent: string;
-  }[];
-
-  stakeholders: {
-    id?: number;
-    stakeholderName: string;
-    stakeholderContent: string;
-  }[];
-
-  risks: {
-    id?: number;
-    riskName: string;
-    riskContent: string;
-  }[];
+  objectives: Objectives[];
+  positions: Positions[];
+  principles: Principles[];
+  scopes: Scopes[];
+  visions: Visions[];
+  stakeholders: Stakeholders[];
+  risks: Risks[];
 }
 
 interface TimelineData {
--- frontend/src/routes/project/ProjectTimelines.tsx
+++ frontend/src/routes/project/ProjectTimelines.tsx
@@ -1,12 +1,15 @@
+import { useProjectDetailInfoQuery } from 'hooks/useProjectDetailInfoQuery';
 import { useParams } from 'react-router-dom';
 import { MS } from 'styles';
 
 export default function ProjectTimelines() {
   const { id } = useParams();
+  const { timelinesQuery } = useProjectDetailInfoQuery(Number(id));
 
   return (
     <div className={MS.container}>
       <p>{id} 프로젝트 타임라인</p>
+      <p>{JSON.stringify(timelinesQuery?.data)}</p>
     </div>
   );
 }
