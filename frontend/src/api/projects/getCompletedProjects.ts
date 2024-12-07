11446996523279a214178ae5a301d874910aadec
Kim Bum Soo <bumsookim333@naver.com>
2024-12-07 16:27:32 +0900

    FE: [refactor] 프로젝트 기반 별 분류 위한 param 추가

--- frontend/src/api/projects/getCompletedProjects.ts
+++ frontend/src/api/projects/getCompletedProjects.ts
@@ -3,14 +3,13 @@ import { tokenAxios } from '../tokenAPI';
 import { API_URL } from '../../constants/envText';
 import { headers } from '../../constants/headers';
 
-export const getAllProject = async () => {
+export const getCompletedProjects = async () => {
   try {
     const response = await tokenAxios.get(`${API_URL}/api/projects`, {
       headers: headers,
-      params: {
-        status: 'ONGOING'
-      }
-
+      //params: {
+        //status: 'COMPLETED'
+      //}
     });
     return response.data;
   } catch (error) {
